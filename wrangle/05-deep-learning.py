158922803e9b19ecb8be0f8ab8016e7ac4cc5679
Joel Grus <joelgrus@gmail.com>
2017-05-14 08:31:38 -0700

    add ipynb

--- wrangle/05-deep-learning.py
+++ wrangle/05-deep-learning.py
@@ -26,12 +26,18 @@ b_h2 = init_weights([NUM_HIDDEN2])
 b_o = init_weights([4])
 
 def model(X, w_h1, w_h2, w_o, b_h1, b_h2, b_o, keep_prob):
-    h1 = tf.nn.dropout(tf.nn.relu(tf.matmul(X, w_h1) + b_h1), keep_prob)
+    if keep_prob < 1.0:
+        h1 = tf.nn.dropout(tf.nn.relu(tf.matmul(X, w_h1) + b_h1), keep_prob)
+    else:
+        h1 = tf.nn.relu(tf.matmul(X, w_h1) + b_h1)
     h2 = tf.nn.relu(tf.matmul(h1, w_h2) + b_h2)
     return tf.matmul(h2, w_o) + b_o
 
 def py_x(keep_prob):
-    return model(data.X, w_h1, w_h2, w_o, b_h1, b_h2, b_o, keep_prob)
+    if keep_prob < 1.0:
+        return tf.identity(model(data.X, w_h1, w_h2, w_o, b_h1, b_h2, b_o, keep_prob), name="train_output")
+    else:
+        return tf.identity(model(data.X, w_h1, w_h2, w_o, b_h1, b_h2, b_o, keep_prob), name="model_output")
 
 keep_prob = args.keep_prob or 0.50
 
@@ -40,7 +46,11 @@ train_op = tf.train.RMSPropOptimizer(learning_rate=0.0003, decay=0.8, momentum=0
 
 predict_op = tf.argmax(py_x(keep_prob=1.0), 1)
 
-with tf.Session() as sess:
-    output = build_model(sess, data, train_op, predict_op, args, cost=cost)
-    learned_parameters = sess.run((w_h1, w_h2, w_o, b_h1, b_h2, b_o))
-    print(output)
+saver = tf.train.Saver()
+
+sess = tf.Session()
+output = build_model(sess, data, train_op, predict_op, args, cost=cost)
+learned_parameters = sess.run((w_h1, w_h2, w_o, b_h1, b_h2, b_o))
+#meta_graph_def = tf.train.export_meta_graph(filename='/tmp/my-model.meta', as_text=True)
+#saver.save(sess, '/tmp/my-model.ckpt')
+print(output)
